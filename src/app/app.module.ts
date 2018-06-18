ed5812e10339dd1a651ea1ebee76529a897176b4
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-18 14:54:33 -0500

    chore(setup): initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {  NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 
-import {  AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
@@ -16,13 +10,9 @@ import {  AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
