5d9e7dac3811df1a7e6a3975e0ec4cbf905a4ed2
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-21 18:34:05 -0500

    feat(dev): added relationships component.

--- src/app/ya-forms/ya-forms.module.ts
+++ src/app/ya-forms/ya-forms.module.ts
@@ -8,12 +8,13 @@ import { YaInterestsComponent } from './ya-interests/ya-interests.component';
 import { YaPlacementsComponent } from './ya-placements/ya-placements.component';
 import { YaParentingComponent } from './ya-parenting/ya-parenting.component';
 import { YaHealthComponent } from './ya-health/ya-health.component';
+import { YaRelationshipsComponent } from './ya-relationships/ya-relationships.component';
 
 @NgModule({
   imports: [
     CommonModule,
     YaFormsRoutingModule
   ],
-  declarations: [YaHousingComponent, YaEducationComponent, YaEmploymentComponent, YaInterestsComponent, YaPlacementsComponent, YaParentingComponent, YaHealthComponent]
+  declarations: [YaHousingComponent, YaEducationComponent, YaEmploymentComponent, YaInterestsComponent, YaPlacementsComponent, YaParentingComponent, YaHealthComponent, YaRelationshipsComponent]
 })
 export class YaFormsModule { }
