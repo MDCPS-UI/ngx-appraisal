721c6044a8d2fedde6c550230728923ec02a1c02
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-21 18:30:27 -0500

    feat(dev): added housing component.

--- src/app/ya-forms/ya-forms.module.ts
+++ src/app/ya-forms/ya-forms.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YaFormsRoutingModule } from './ya-forms-routing.module';
+import { YaHousingComponent } from './ya-housing/ya-housing.component';
 
 @NgModule({
   imports: [
     CommonModule,
     YaFormsRoutingModule
   ],
-  declarations: []
+  declarations: [YaHousingComponent]
 })
 export class YaFormsModule { }
---
+++ src/app/ya-forms/ya-housing/ya-housing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mdcps-ya-housing',
+  templateUrl: './ya-housing.component.html',
+  styleUrls: ['./ya-housing.component.scss']
+})
+export class YaHousingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
