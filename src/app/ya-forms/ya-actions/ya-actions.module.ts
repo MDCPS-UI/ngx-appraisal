2fd13724f245f4eb48b18882cb9b55ed422dc125
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-22 07:17:26 -0500

    feat(routes): lazy loading WIP.

--- src/app/ya-forms/ya-actions/ya-actions.module.ts
+++ src/app/ya-forms/ya-actions/ya-actions.module.ts
@@ -1,10 +1,35 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+// Components
+import { YaActionsComponent } from './ya-actions.component';
+import { BaseFormComponent } from './../../shared/components/base-form/base-form.component';
+
+// YA Action Form Routes
+const ACTION_FORM_ROUTES: Route[] = [
+  {
+    path: '',
+    component: YaActionsComponent
+  }
+];
+
+/**
+ * @author: Shoukath Mohammed
+ */
 @NgModule({
   imports: [
-    CommonModule
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(ACTION_FORM_ROUTES)
   ],
-  declarations: []
+  declarations: [
+    BaseFormComponent,
+    YaActionsComponent
+  ],
+  exports: [RouterModule]
 })
 export class YaActionsModule { }
--- src/app/ya-forms/ya-forms-routing.module.ts
+++ src/app/ya-forms/ya-forms-routing.module.ts
@@ -10,7 +10,6 @@ import { YaFormsComponent } from './ya-forms.component';
 import { YaReviewComponent } from './ya-review/ya-review.component';
 import { YaHealthComponent } from './ya-health/ya-health.component';
 import { YaSkillsComponent } from './ya-skills/ya-skills.component';
-import { YaActionsComponent } from './ya-actions/ya-actions.component';
 import { YaHousingComponent } from './ya-housing/ya-housing.component';
 import { YaStipendsComponent } from './ya-stipends/ya-stipends.component';
 import { YaServicesComponent } from './ya-services/ya-services.component';
@@ -22,139 +21,139 @@ import { YaInterestsComponent } from './ya-interests/ya-interests.component';
 import { YaParentingComponent } from './ya-parenting/ya-parenting.component';
 import { YaEmploymentComponent } from './ya-employment/ya-employment.component';
 import { YaPlacementsComponent } from './ya-placements/ya-placements.component';
-import { YaDemographicsComponent } from './ya-demographics/ya-demographics.component';
 import { YaRelationshipsComponent } from './ya-relationships/ya-relationships.component';
-import { BaseFormComponent } from './../shared/components/base-form/base-form.component';
 
 // YA Routes
-const ROUTES: Route[] = [{
-  path: '',
-  component: YaFormsComponent,
-  children: [
-    {
-      path: '',
-      redirectTo: 'demographics'
-    },
-    {
-      path: 'demographics',
-      component: YaDemographicsComponent,
-      data: {
-        formName: 'Demographics'
-      }
-    },
-    {
-      path: 'housing',
-      component: YaHousingComponent,
-      data: {
-        formName: 'Housing'
-      }
-    },
-    {
-      path: 'education',
-      component: YaEducationComponent,
-      data: {
-        formName: 'Education'
-      }
-    },
-    {
-      path: 'employment',
-      component: YaEmploymentComponent,
-      data: {
-        formName: 'Employment'
-      }
-    },
-    {
-      path: 'interests',
-      component: YaInterestsComponent,
-      data: {
-        formName: 'Social Interests'
-      }
-    },
-    {
-      path: 'placements',
-      component: YaPlacementsComponent,
-      data: {
-        formName: 'Placement & Safety'
-      }
-    },
-    {
-      path: 'parenting',
-      component: YaParentingComponent,
-      data: {
-        formName: 'Pregnant/Parenting'
-      }
-    },
-    {
-      path: 'health',
-      component: YaHealthComponent,
-      data: {
-        formName: 'Health'
-      }
-    },
-    {
-      path: 'relationships',
-      component: YaRelationshipsComponent,
-      data: {
-        formName: 'Relationships'
-      }
-    },
-    {
-      path: 'criminal',
-      component: YaCriminalComponent,
-      data: {
-        formName: 'Criminal Justice System'
-      }
-    },
-    {
-      path: 'skills',
-      component: YaSkillsComponent,
-      data: {
-        formName: 'Life Skills'
-      }
-    },
-    {
-      path: 'stipends',
-      component: YaStipendsComponent,
-      data: {
-        formName: 'Stipends'
-      }
-    },
-    {
-      path: 'services',
-      component: YaServicesComponent,
-      data: {
-        formName: 'Continuing Support/Services'
-      }
-    },
-    {
-      path: 'personal',
-      component: YaPersonalComponent,
-      data: {
-        formName: 'Personal'
-      }
-    },
-    {
-      path: 'actions',
-      component: YaActionsComponent,
-      data: {
-        formName: 'Goals and Actions'
-      }
-    },
-    {
-      path: 'documents',
-      component: YaDocumentsComponent,
-      data: {
-        formName: 'Received Documents'
-      }
-    },
-    {
-      path: 'review',
-      component: YaReviewComponent,
-      data: {
-        formName: 'Review & Sign'
-      }
-    }]
-}];
+const ROUTES: Route[] = [
+  {
+    path: '',
+    component: YaFormsComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'demographics'
+      },
+      {
+        path: 'demographics',
+        loadChildren: './ya-demographics/ya-demographics.module#YaDemographicsModule',
+        data: {
+          formName: 'Demographics'
+        }
+      },
+      {
+        path: 'housing',
+        component: YaHousingComponent,
+        data: {
+          formName: 'Housing'
+        }
+      },
+      {
+        path: 'education',
+        component: YaEducationComponent,
+        data: {
+          formName: 'Education'
+        }
+      },
+      {
+        path: 'employment',
+        component: YaEmploymentComponent,
+        data: {
+          formName: 'Employment'
+        }
+      },
+      {
+        path: 'interests',
+        component: YaInterestsComponent,
+        data: {
+          formName: 'Social Interests'
+        }
+      },
+      {
+        path: 'placements',
+        component: YaPlacementsComponent,
+        data: {
+          formName: 'Placement & Safety'
+        }
+      },
+      {
+        path: 'parenting',
+        component: YaParentingComponent,
+        data: {
+          formName: 'Pregnant/Parenting'
+        }
+      },
+      {
+        path: 'health',
+        component: YaHealthComponent,
+        data: {
+          formName: 'Health'
+        }
+      },
+      {
+        path: 'relationships',
+        component: YaRelationshipsComponent,
+        data: {
+          formName: 'Relationships'
+        }
+      },
+      {
+        path: 'criminal',
+        component: YaCriminalComponent,
+        data: {
+          formName: 'Criminal Justice System'
+        }
+      },
+      {
+        path: 'skills',
+        component: YaSkillsComponent,
+        data: {
+          formName: 'Life Skills'
+        }
+      },
+      {
+        path: 'stipends',
+        component: YaStipendsComponent,
+        data: {
+          formName: 'Stipends'
+        }
+      },
+      {
+        path: 'services',
+        component: YaServicesComponent,
+        data: {
+          formName: 'Continuing Support/Services'
+        }
+      },
+      {
+        path: 'personal',
+        component: YaPersonalComponent,
+        data: {
+          formName: 'Personal'
+        }
+      },
+      {
+        path: 'actions',
+        loadChildren: './ya-actions/ya-actions.module#YaActionsModule',
+        data: {
+          formName: 'Goals and Actions'
+        }
+      },
+      {
+        path: 'documents',
+        component: YaDocumentsComponent,
+        data: {
+          formName: 'Received Documents'
+        }
+      },
+      {
+        path: 'review',
+        component: YaReviewComponent,
+        data: {
+          formName: 'Review & Sign'
+        }
+      }]
+  }
+];
 
 /**
  * @author: Shoukath Mohammed
@@ -170,11 +169,9 @@ const ROUTES: Route[] = [{
   declarations: [
     YaFormsComponent,
     YaReviewComponent,
-    BaseFormComponent,
     YaHealthComponent,
     YaSkillsComponent,
     YaHousingComponent,
-    YaActionsComponent,
     YaCriminalComponent,
     YaStipendsComponent,
     YaServicesComponent,
@@ -185,7 +182,7 @@ const ROUTES: Route[] = [{
     YaInterestsComponent,
     YaPlacementsComponent,
     YaEmploymentComponent,
-    YaDemographicsComponent,
+
     YaRelationshipsComponent
   ],
   exports: [RouterModule]
