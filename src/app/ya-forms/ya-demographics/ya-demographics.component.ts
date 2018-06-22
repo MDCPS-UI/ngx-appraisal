d02c3918a7f3eac645ed3bdd4d1ff688adcc1a10
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-22 05:56:00 -0500

    feat(dev): demographics form implementation WIP.

--- src/app/ya-forms/ya-demographics/ya-demographics.component.ts
+++ src/app/ya-forms/ya-demographics/ya-demographics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'mdcps-ya-demographics',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YaDemographicsComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * @public
+   */
+  public demographicsForm: FormGroup;
+
+  /**
+   * @constructor
+   * @param {fb<FormBuilder>}
+   */
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.demographicsForm = this.fb.group({
+      dateOfBirth: new FormControl('', []),
+      age: new FormControl('', []),
+      phone: new FormControl('', []),
+      email: new FormControl('', []),
+      permenancygoal: new FormControl('', []),
+      custodystatus: new FormControl('', []),
+      dischargeDate: new FormControl('', []),
+      address: new FormControl('', []),
+      race: new FormControl('', []),
+      gender: new FormControl('', []),
+      LGBTQ: new FormControl('', []),
+      COR: new FormControl('', []),
+      COS: new FormControl('', []),
+      healthInsurance: new FormControl('', []),
+      medicaid: new FormControl('', []),
+      languageBarriers: new FormControl('', []),
+      explainLanguageBarriers: new FormControl('', []),
+      mentor: new FormControl('', []),
+      transitionplanning: new FormControl('', []),
+      corWorker: new FormControl('', [])
+    });
   }
 
+  /**
+   * @public
+   */
+  public onSubmit(form: FormGroup, value: any): void {
+    if (form.valid) {
+      console.log(form.value);
+    }
+  }
 }
--- src/app/ya-forms/ya-forms-routing.module.ts
+++ src/app/ya-forms/ya-forms-routing.module.ts
@@ -1,6 +1,7 @@
 // Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Route } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -161,6 +162,7 @@ const ROUTES: Route[] = [{
   imports: [
     NgbModule,
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(ROUTES)
   ],
   declarations: [
