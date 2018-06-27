da5551d5d7a9e3461f2385ccaa26a0eaa6f264a0
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-27 18:30:14 -0500

    feat(dev): removed duplicated code.

---
+++ src/app/ya-forms/ya-skills/ya-skills.constants.ts
@@ -0,0 +1,136 @@
+/**
+ * @type: constant
+ * @author: Shoukath Mohammed
+ */
+export const YA_SKILLS_LIST: any[] = [
+    {
+        title: 'Please indicate which of the following skills apply?',
+        skills: [
+            {
+                optionName: 'emailSkills',
+                label: 'I know how to use my email account',
+                value: '01'
+            },
+            {
+                optionName: 'cookingSkills',
+                label: 'I can make meals without using a recipe',
+                value: '02'
+            },
+            {
+                optionName: 'laundrySkills',
+                label: 'I know how to do my own laundry',
+                value: '03'
+            },
+            {
+                optionName: 'medicalCareSkills',
+                label: 'I can get medical and dental care when I need it',
+                value: '04'
+            },
+            {
+                optionName: 'bathingSkills',
+                label: 'I bathe daily',
+                value: '05'
+            },
+            {
+                optionName: 'brushingSkills',
+                label: 'I brush my teeth daily',
+                value: '06'
+            },
+            {
+                optionName: 'protectionSkills',
+                label: 'I know how to get myself away from harmful situations',
+                value: '07'
+            },
+            {
+                optionName: 'speakingSkills',
+                label: 'I can speak up for myself',
+                value: '08'
+            },
+            {
+                optionName: 'abusiveRelationSkills',
+                label: 'I know the signs of an abusive relationship',
+                value: '08'
+            },
+            {
+                optionName: 'angerManagementSkills',
+                label: 'I can deal with anger without hurting myself or others',
+                value: '09'
+            },
+            {
+                optionName: 'finacialSkills',
+                label: 'I know how to balance my bank account',
+                value: '10'
+            },
+            {
+                optionName: 'budgetSkills',
+                label: 'I know how to develop a budget',
+                value: '11'
+            },
+            {
+                optionName: 'resumeSkills',
+                label: 'I know how to create a resume',
+                value: '12'
+            },
+            {
+                optionName: 'jobApplicationSkills',
+                label: 'I can complete a job application',
+                value: '13'
+            },
+            {
+                optionName: 'jobInterviewSkills',
+                label: 'I know how to prepare for a job interview',
+                value: '14'
+            },
+            {
+                optionName: 'visionSkills',
+                label: 'I can describe my vision for myself',
+                value: '15'
+            },
+            {
+                optionName: 'drivingSkills',
+                label: 'I know how to drive',
+                value: '16'
+            }
+        ]
+    },
+    {
+        title: 'Which of the following Independent Living Services have you received?',
+        skills: [
+            {
+                optionName: 'educationalSupport',
+                label: 'Educational Support',
+                value: '01'
+            },
+            {
+                optionName: 'employmentServices',
+                label: 'Employment Services',
+                value: '02'
+            },
+            {
+                optionName: 'healthCare',
+                label: 'Health Care',
+                value: '03'
+            },
+            {
+                optionName: 'moneyManagement',
+                label: 'Money Management',
+                value: '04'
+            },
+            {
+                optionName: 'housingServices',
+                label: 'Housing',
+                value: '05'
+            },
+            {
+                optionName: 'afterCare',
+                label: 'After Care',
+                value: '06'
+            },
+            {
+                optionName: 'etvServices',
+                label: 'ETV',
+                value: '07'
+            }
+        ]
+    }
+];
