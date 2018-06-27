9f49251943017961d49e2aee9d3543876a2b45c2
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-06-27 01:13:24 -0500

    feat(dev): implemented typeahead functionality.

--- src/app/ya-dashboard/ya-dashboard.module.ts
+++ src/app/ya-dashboard/ya-dashboard.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Route, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Route, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { YaDashboardComponent } from './ya-dashboard.component';
 import { MaterialModule } from './../shared/modules/material/material.module';
 import { HeaderComponent } from './../shared/components/layout/header/header.component';
@@ -27,6 +28,7 @@ const ROUTES: Route[] = [
  */
 @NgModule({
   imports: [
+    NgbModule,
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
