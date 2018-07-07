306b33511269aa8a3d10d7092b69098e9189dc47
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-07-07 01:54:37 -0500

    feat(dev): relationships form checkboxes implementation.

--- src/app/ya-forms/ya-relationships/ya-relationships.constants.ts
+++ src/app/ya-forms/ya-relationships/ya-relationships.constants.ts
@@ -2,10 +2,10 @@
  * @type: constant
  * @author: Shoukath Mohammed
  */
-export const YA_REL_LIST: any[] = [
+export const YA_RELSHP_LIST: any[] = [
     {
         'desc': 'Are you currently in a relationship with someone?',
-        'optionName': ''
+        'optionName': 'inRelationship'
     },
     {
         'desc': 'Are you currently in a relationship with a partner who is ___________ abusive?',
@@ -13,57 +13,44 @@ export const YA_REL_LIST: any[] = [
         'isCheckbox': true,
         'options': [
             {
-                'label': '',
-                'value': ''
+                'label': 'Physically',
+                'value': 'physically',
+                'optionName': 'physically'
             },
             {
-                'label': '',
-                'value': ''
+                'label': 'Emotionally',
+                'value': 'emotionally',
+                'optionName': 'physically'
             },
             {
-                'label': '',
-                'value': ''
+                'label': 'Verbally',
+                'value': 'verbally',
+                'optionName': 'physically'
             }
         ]
     },
     {
-        'desc': '',
-        'optionName': ''
+        'desc': 'Do you ever feel afraid of a current or former partner?',
+        'optionName': 'afraidOfPartner'
     },
     {
-        'desc': '',
-        'optionName': ''
+        'desc': 'Are you currently leaving a violent or threatening relationship?',
+        'optionName': 'violentRelationship'
     },
     {
-        'desc': '',
-        'optionName': ''
+        'desc': 'Have you ever received housing, food or money for sexual services?',
+        'optionName': 'compensationForSexualServices'
     },
     {
-        'desc': '',
-        'optionName': ''
+        'desc': 'Have you ever hurt or threatened someone to have sex with you or tried to have sexual contact with someone against their will?',
+        'optionName': 'forcedSexualContact'
     },
     {
-        'desc': '',
-        'optionName': ''
+        'desc': 'Has anyone ever tried to have sex with you against your will?',
+        'optionName': 'forcedToHaveSex'
     },
     {
-        'desc': '',
-        'optionName': ''
-    },
-    {
-        'desc': '',
-        'optionName': ''
-    },
-    {
-        'desc': '',
-        'optionName': ''
-    },
-    {
-        'desc': '',
-        'optionName': ''
-    },
-    {
-        'desc': '',
-        'optionName': ''
+        'desc': 'Are you currently involved in prostitution or pimping?',
+        'optionName': 'involvedInProstitution'
     }
 ];
