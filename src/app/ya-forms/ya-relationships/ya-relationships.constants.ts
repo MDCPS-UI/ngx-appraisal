c5f4e6ee70ac9b2ebb1b8399de3694ad89480112
Mohammed9531 <mohd.shoukath7866@gmail.com>
2018-07-06 21:48:38 -0500

    feat(dev): relationship form - WIP.

---
+++ src/app/ya-forms/ya-relationships/ya-relationships.constants.ts
@@ -0,0 +1,69 @@
+/**
+ * @type: constant
+ * @author: Shoukath Mohammed
+ */
+export const YA_REL_LIST: any[] = [
+    {
+        'desc': 'Are you currently in a relationship with someone?',
+        'optionName': ''
+    },
+    {
+        'desc': 'Are you currently in a relationship with a partner who is ___________ abusive?',
+        'optionName': 'abusivePartner',
+        'isCheckbox': true,
+        'options': [
+            {
+                'label': '',
+                'value': ''
+            },
+            {
+                'label': '',
+                'value': ''
+            },
+            {
+                'label': '',
+                'value': ''
+            }
+        ]
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    },
+    {
+        'desc': '',
+        'optionName': ''
+    }
+];
